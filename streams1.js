5c6b18660ac43aac0159405dce18377f115e9675
Ben Rockwood <benr@joyent.com>
2014-06-09 00:51:01 -0700

    Added clarification in streams1 and fixed a typo

--- streams1.js
+++ streams1.js
@@ -1,4 +1,5 @@
-// Stream 2: This just acts like "cat"
+// Stream 1: This just acts like "cat"
+// This isn't really a stream at all, its just an event handler on a Readable Stream
 
 var fs = require('fs')
 var source = fs.createReadStream('/etc/passwd'); // Readable Stream
